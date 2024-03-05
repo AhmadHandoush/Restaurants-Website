57c5db15fd1a24a3fe7ccabbf6709ca36136d2d8
hamada <hamadahandoush9991@gmail.com>
2024-03-06 00:15:40 +0200

    login page html and css

--- js/users.js
+++ js/users.js
@@ -9,12 +9,10 @@ if (users) {
     let emailPlace = document.createElement("td");
     let passwordPlace = document.createElement("td");
 
-    // Populate the cells with user data
     userPlace.textContent = user.name;
     emailPlace.textContent = user.email;
     passwordPlace.textContent = user.password;
 
-    // Append cells to the row
     row.appendChild(userPlace);
     row.appendChild(emailPlace);
     row.appendChild(passwordPlace);
